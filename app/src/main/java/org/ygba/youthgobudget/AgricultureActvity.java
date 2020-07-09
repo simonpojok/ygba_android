79901fa59f034c05db693adf7e8891ad065f29ba
simonojok19 <simonojok19@gmail.com>
2020-07-09 13:47:32 +0300

    added agriculture activity

---
+++ app/src/main/java/org/ygba/youthgobudget/AgricultureActvity.java
@@ -0,0 +1,14 @@
+package org.ygba.youthgobudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgricultureActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agriculture_actvity);
+    }
+}
