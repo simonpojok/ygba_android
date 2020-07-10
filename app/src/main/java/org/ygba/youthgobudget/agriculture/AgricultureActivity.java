9e458ead0e8e6b593c8b93f737333242ddaa50c6
simonojok19 <simonojok19@gmail.com>
2020-07-10 16:22:18 +0300

    woking on binding data

--- app/src/main/java/org/ygba/youthgobudget/agriculture/AgricultureActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/agriculture/AgricultureActivity.java
@@ -3,12 +3,15 @@ package org.ygba.youthgobudget.agriculture;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import org.ygba.youthgobudget.R;
 
 public class AgricultureActivity extends AppCompatActivity {
     Spinner financialYearSpinner;
+    EditText villageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +19,6 @@ public class AgricultureActivity extends AppCompatActivity {
         setContentView(R.layout.activity_agriculture_activity);
 
         financialYearSpinner = findViewById(R.id.financial_year_spinner);
+        villageEditText = findViewById(R.id.village_text_edit);
     }
 }
