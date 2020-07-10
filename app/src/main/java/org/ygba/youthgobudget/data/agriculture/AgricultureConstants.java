9bebddf67575de37177541895805b953d215a2e3
simonojok19 <simonojok19@gmail.com>
2020-07-10 18:26:11 +0300

    added several entities

---
+++ app/src/main/java/org/ygba/youthgobudget/data/agriculture/AgricultureConstants.java
@@ -0,0 +1,4 @@
+package org.ygba.youthgobudget.data.agriculture;
+
+public class AgricultureConstants {
+}
---
+++ app/src/main/java/org/ygba/youthgobudget/data/agriculture/AgricultureDao.java
@@ -0,0 +1,4 @@
+package org.ygba.youthgobudget.data.agriculture;
+
+public interface AgricultureDao {
+}
