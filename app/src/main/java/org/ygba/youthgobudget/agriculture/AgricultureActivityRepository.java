e2cf21983de1221cd7db4a57280d499fc5dc794c
simonojok19 <simonojok19@gmail.com>
2020-07-11 10:52:16 +0300

    adding repository done

--- app/src/main/java/org/ygba/youthgobudget/agriculture/AgricultureActivityRepository.java
+++ app/src/main/java/org/ygba/youthgobudget/agriculture/AgricultureActivityRepository.java
@@ -7,6 +7,7 @@ import androidx.lifecycle.AndroidViewModel;
 
 import org.ygba.youthgobudget.YGBARepository;
 import org.ygba.youthgobudget.data.YGBDatabase;
+import org.ygba.youthgobudget.data.agriculture.AgricultureQuestion;
 
 public class AgricultureActivityRepository extends AndroidViewModel {
     YGBARepository ygbaRepository;
@@ -14,5 +15,9 @@ public class AgricultureActivityRepository extends AndroidViewModel {
     public AgricultureActivityRepository(@NonNull Application application) {
         super(application);
         ygbaRepository = YGBARepository.getInstance(YGBDatabase.getInstance(application));
+    }
+
+    public void saveAgricultureQuestion(AgricultureQuestion agricultureQuestion) {
+        ygbaRepository.saveAgricultureQuestion(agricultureQuestion);
     }
 }
