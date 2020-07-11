8e44e0eb75a91e2619963d351f7224b2ab03e376
simonojok19 <simonojok19@gmail.com>
2020-07-11 13:31:46 +0300

    working on the education form

--- app/src/main/java/org/ygba/youthgobudget/data/education/EducationQuestion.java
+++ app/src/main/java/org/ygba/youthgobudget/data/education/EducationQuestion.java
@@ -1,4 +1,15 @@
 package org.ygba.youthgobudget.data.education;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = EducationQuestionConstants.TABLE_NAME)
 public class EducationQuestion {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = EducationQuestionConstants.PRIMARY_KEY)
+    private  int id;
+
+    @ColumnInfo(name = EducationQuestionConstants.FINANCIAL_YEAR)
+    private String financialYear;
 }
--- app/src/main/java/org/ygba/youthgobudget/data/education/EducationQuestionConstants.java
+++ app/src/main/java/org/ygba/youthgobudget/data/education/EducationQuestionConstants.java
@@ -1,4 +1,7 @@
 package org.ygba.youthgobudget.data.education;
 
 public class EducationQuestionConstants {
+    public static final String TABLE_NAME = "educ_table";
+    public static final String FINANCIAL_YEAR = "educ_financial";
+    public static final String PRIMARY_KEY = "pri";
 }
