3e105b555595050c579d90242ec6dd0005f4d75b
simonojok19 <simonojok19@gmail.com>
2020-07-11 10:37:01 +0300

    databse set up done

--- app/src/main/java/org/ygba/youthgobudget/data/YGBDatabase.java
+++ app/src/main/java/org/ygba/youthgobudget/data/YGBDatabase.java
@@ -9,14 +9,24 @@ import androidx.room.RoomDatabase;
 import org.ygba.youthgobudget.data.agriculture.AgricultureDao;
 import org.ygba.youthgobudget.data.agriculture.AgricultureQuestion;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Database(entities = {AgricultureQuestion.class}, version = 1)
 public abstract class YGBDatabase extends RoomDatabase {
     private static YGBDatabase INSTANCE;
     public abstract AgricultureDao agricultureDao();
+    public static final int NUMBER_OF_THREADS = 5;
+    public static final ExecutorService db_executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
 
     public static YGBDatabase getInstance(Context context) {
         if (INSTANCE == null ) {
-            INSTANCE = Room.databaseBuilder(context, YGBDatabase.class, "ygba-db").build();
+           synchronized (YGBDatabase.class) {
+               if (INSTANCE == null) {
+                   INSTANCE = Room.databaseBuilder(context.getApplicationContext(), YGBDatabase.class, "ygba-db").build();
+               }
+           }
         }
         return INSTANCE;
     }
