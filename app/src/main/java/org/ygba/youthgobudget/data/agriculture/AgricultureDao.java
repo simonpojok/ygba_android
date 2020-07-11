fcdd3ef5bd6a9c85ee1c80bfcaa3160f78affae4
simonojok19 <simonojok19@gmail.com>
2020-07-11 10:22:34 +0300

    added the dao complete

--- app/src/main/java/org/ygba/youthgobudget/data/agriculture/AgricultureDao.java
+++ app/src/main/java/org/ygba/youthgobudget/data/agriculture/AgricultureDao.java
@@ -1,4 +1,19 @@
 package org.ygba.youthgobudget.data.agriculture;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface AgricultureDao {
+    @Insert
+    void saveAgricultureQuestion(AgricultureQuestion agricultureQuestion);
+
+    @Query(
+            "SELECT * FROM " + AgricultureConstants.TABLE_NAME
+    )
+    LiveData<List<AgricultureQuestion>> getAllAgricultureAnswers();
 }
