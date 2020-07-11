dc3827a8ef6643dd2dd644464aa7f73d4eb989a9
simonojok19 <simonojok19@gmail.com>
2020-07-11 10:41:55 +0300

    added singleton repository

--- app/src/main/java/org/ygba/youthgobudget/YGBARepository.java
+++ app/src/main/java/org/ygba/youthgobudget/YGBARepository.java
@@ -1,4 +1,23 @@
 package org.ygba.youthgobudget;
 
+import org.ygba.youthgobudget.data.YGBDatabase;
+
 public class YGBARepository {
+    private static YGBARepository INSTANCE;
+    private YGBDatabase ygbDatabase;
+
+    private YGBARepository(YGBDatabase ygbDatabase) {
+        this.ygbDatabase = ygbDatabase;
+    }
+
+    private YGBARepository getInstance(YGBDatabase ygbDatabase){
+        if (INSTANCE == null ) {
+            synchronized (YGBARepository.class) {
+                if (INSTANCE == null ) {
+                    INSTANCE = new YGBARepository(ygbDatabase);
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
