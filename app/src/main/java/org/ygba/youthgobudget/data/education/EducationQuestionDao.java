f4b7ab0b6e504dd9add2950490aaf39fef4b0e2b
simonojok19 <simonojok19@gmail.com>
2020-07-11 17:10:09 +0300

    added the dao

--- app/src/main/java/org/ygba/youthgobudget/data/education/EducationQuestionDao.java
+++ app/src/main/java/org/ygba/youthgobudget/data/education/EducationQuestionDao.java
@@ -1,4 +1,15 @@
 package org.ygba.youthgobudget.data.education;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface EducationQuestionDao {
+    void saveEducationQuestion(EducationQuestion educationQuestion);
+
+    @Query("SELECT * FROM " + EducationQuestionConstants.TABLE_NAME)
+    LiveData<List<EducationQuestion>> getAllEducationQuestions();
 }
