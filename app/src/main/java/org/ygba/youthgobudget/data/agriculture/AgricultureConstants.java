480a1c71f199cc7ea278cbb23cf46f1a92ac22c6
simonojok19 <simonojok19@gmail.com>
2020-07-11 09:37:11 +0300

    started adding the schema for database

--- app/src/main/java/org/ygba/youthgobudget/data/agriculture/AgricultureConstants.java
+++ app/src/main/java/org/ygba/youthgobudget/data/agriculture/AgricultureConstants.java
@@ -1,4 +1,24 @@
 package org.ygba.youthgobudget.data.agriculture;
 
 public class AgricultureConstants {
+    public static final String TABLE_NAME = "agric";
+    public static final String PRIMARY_KEY = "id";
+    public static final String FINANCIAL_YEAR = "fy";
+    public static final String DATE = "date";
+    public static final String VILLAGE = "village";
+    public static final String PARISH = "parish";
+    public static final String SUB_COUNTY = "sub_county";
+    public static final String AGENT_NAME = "name";
+    public static final String TELEPHONE = "tel";
+    public static final String NUMBER = "num";
+    public static final String QUESTION_1_OBJECTIVE = "q1o";
+    public static final String QUESTION_1_REASON = "q1reason";
+    public static final String QUESTION_2_EXT_EXPECTED = "q2ext";
+    public static final String QUESTION_2_EXT_RECEIVED = "q2extreceived";
+    public static final String QUESTION_2_EXT_DATE_RECEIVED = "q2dateextReceived";
+    public static final String QUESTION_2_EXT_DATE_WITHDRAWN = "q2extdatewithdrawn";
+    public static final String QUESTION_2_DEV_EXPECTED_AMOUNT = "q2devexp";
+    public static final String QUESTION_2_DEV_AMOUNT_RECEIVED = "q2devrecieved";
+    public static final String QUESTION_2_DEV_DATE_RECEIVED = "q2devdaterecieved";
+    public static final String QUESTION_2_DEV_DATE_WITHDRAWN = "q2devdatewithdrawn";
 }
--- app/src/main/java/org/ygba/youthgobudget/data/agriculture/AgricultureQuestion.java
+++ app/src/main/java/org/ygba/youthgobudget/data/agriculture/AgricultureQuestion.java
@@ -1,4 +1,66 @@
 package org.ygba.youthgobudget.data.agriculture;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = AgricultureConstants.TABLE_NAME)
 public class AgricultureQuestion {
+    @ColumnInfo(name = AgricultureConstants.PRIMARY_KEY)
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = AgricultureConstants.FINANCIAL_YEAR)
+    private String financialYear;
+
+    @ColumnInfo(name = AgricultureConstants.DATE)
+    private String date;
+
+    @ColumnInfo(name = AgricultureConstants.VILLAGE)
+    private String village;4
+
+    @ColumnInfo(name = AgricultureConstants.PARISH)
+    private String parish;
+
+    @ColumnInfo(name = AgricultureConstants.SUB_COUNTY)
+    private String subCounty;
+
+    @ColumnInfo(name = AgricultureConstants.AGENT_NAME)
+    private String agentName;
+
+    @ColumnInfo(name = AgricultureConstants.TELEPHONE)
+    private String tel;
+
+    @ColumnInfo(name = AgricultureConstants.NUMBER)
+    private String agentNumber;
+
+    @ColumnInfo(name = AgricultureConstants.QUESTION_1_OBJECTIVE)
+    private String question1Objective;
+
+    @ColumnInfo(name = AgricultureConstants.QUESTION_1_REASON)
+    private String question1Reason;
+
+    @ColumnInfo(name = AgricultureConstants.QUESTION_2_EXT_EXPECTED)
+    private String question2ExtensionExpectedAmount;
+
+    @ColumnInfo(name = AgricultureConstants.QUESTION_2_EXT_RECEIVED)
+    private String question2ExtensionAmountReceived;
+
+    @ColumnInfo(name = AgricultureConstants.QUESTION_2_EXT_DATE_RECEIVED)
+    private String question2ExtensionDateReceived;
+
+    @ColumnInfo(name = AgricultureConstants.QUESTION_2_EXT_DATE_WITHDRAWN)
+    private String question2ExtensionDateWithdram;
+
+    @ColumnInfo(name = AgricultureConstants.QUESTION_2_DEV_EXPECTED_AMOUNT)
+    private String question2DevelopmentAmountExpected;
+
+    @ColumnInfo(name = AgricultureConstants.QUESTION_2_DEV_AMOUNT_RECEIVED)
+    private String question2DevelopmentAmountReceived;
+
+    @ColumnInfo(name = AgricultureConstants.QUESTION_2_DEV_DATE_RECEIVED)
+    private String question2DevelopmentDateReceived;
+
+    @ColumnInfo(name = AgricultureConstants.QUESTION_2_DEV_DATE_WITHDRAWN)
+    private String question2DevelopmentDateWithdrawn;
 }
