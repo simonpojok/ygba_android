dda5fa337b928b0535933fd294c1e280294e4bee
simonojok19 <simonojok19@gmail.com>
2020-07-12 13:55:53 +0300

    added some codes

--- app/src/main/java/org/ygba/youthgobudget/dialogs/DatePickerActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/dialogs/DatePickerActivity.java
@@ -8,6 +8,8 @@ import org.ygba.youthgobudget.R;
 
 public class DatePickerActivity extends AppCompatActivity {
 
+    public static final String SELECTED_DATE = "org.ygba.youthgobudget.dialogs.SELECTED_DATE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/org/ygba/youthgobudget/socialdevelopment/SocialDevelopmentActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/socialdevelopment/SocialDevelopmentActivity.java
@@ -1,16 +1,21 @@
 package org.ygba.youthgobudget.socialdevelopment;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.ygba.youthgobudget.R;
+import org.ygba.youthgobudget.dialogs.DatePickerActivity;
 
 public class SocialDevelopmentActivity extends AppCompatActivity {
+    private final int COMMUNITY_DATE_WITHDRAWN_REQUEST_CODE = 1;
     private SocialDevelopmentActivityViewModel activityViewModel;
     private TextView socialDateTextView;
     private Spinner financialYearSpinner;
@@ -35,6 +40,17 @@ public class SocialDevelopmentActivity extends AppCompatActivity {
         activityViewModel = new ViewModelProvider(this).get(SocialDevelopmentActivityViewModel.class);
 
         initViews();
+
+        setOnClickListeners();
+    }
+
+    private void setOnClickListeners() {
+        otherDateWithdrawnEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(SocialDevelopmentActivity.this, DatePickerActivity.class), COMMUNITY_DATE_WITHDRAWN_REQUEST_CODE);
+            }
+        });
     }
 
     private void initViews() {
@@ -54,5 +70,15 @@ public class SocialDevelopmentActivity extends AppCompatActivity {
         otherDateReceivedEditText = findViewById(R.id.other_date_received_edit_text);
         otherDateWithdrawnEditText =  findViewById(R.id.other_date_withdrawn_edit_text);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if ((data != null) && (resultCode == RESULT_OK )) {
+            switch (requestCode) {
+                case COMMUNITY_DATE_WITHDRAWN_REQUEST_CODE:
+                    otherDateWithdrawnEditText.setText(data.getStringExtra(DatePickerActivity.SELECTED_DATE));
+            }
+        }
     }
 }
