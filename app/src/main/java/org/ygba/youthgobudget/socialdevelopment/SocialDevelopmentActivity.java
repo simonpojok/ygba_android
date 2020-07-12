1aabf5caf5adb4ea5d3276af2354bc68e35faa71
simonojok19 <simonojok19@gmail.com>
2020-07-12 11:53:25 +0300

    connected the view model

--- app/src/main/java/org/ygba/youthgobudget/socialdevelopment/SocialDevelopmentActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/socialdevelopment/SocialDevelopmentActivity.java
@@ -1,16 +1,20 @@
 package org.ygba.youthgobudget.socialdevelopment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 import org.ygba.youthgobudget.R;
 
 public class SocialDevelopmentActivity extends AppCompatActivity {
+    private SocialDevelopmentActivityViewModel activityViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_development);
+
+        activityViewModel = new ViewModelProvider(this).get(SocialDevelopmentActivityViewModel.class);
     }
 }
