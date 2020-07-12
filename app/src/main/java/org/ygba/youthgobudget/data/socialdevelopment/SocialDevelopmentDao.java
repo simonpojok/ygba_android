0a2dc8ffee29e60e2f2d0f0c4147809e35a46160
simonojok19 <simonojok19@gmail.com>
2020-07-12 23:29:22 +0300

    dao added sone4

--- app/src/main/java/org/ygba/youthgobudget/data/socialdevelopment/SocialDevelopmentDao.java
+++ app/src/main/java/org/ygba/youthgobudget/data/socialdevelopment/SocialDevelopmentDao.java
@@ -1,4 +1,17 @@
 package org.ygba.youthgobudget.data.socialdevelopment;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface SocialDevelopmentDao {
+    @Insert
+    void saveSocialDevelopmentQuestion(SocialDevelopmentQuestion socialDevelopmentQuestion);
+
+    @Query("SELECT * FROM " + SocialDevelopmentConstants.TABLE_NAME)
+    LiveData<List<SocialDevelopmentQuestion>> getAllSocialDevelopmentQuestions();
 }
