1cb4da5523f6b1d1b764004b41704f3859aa2aa8
simonojok19 <simonojok19@gmail.com>
2020-07-12 14:42:47 +0300

    date picker complete

--- app/src/main/java/org/ygba/youthgobudget/dialogs/DatePickerActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/dialogs/DatePickerActivity.java
@@ -3,7 +3,10 @@ package org.ygba.youthgobudget.dialogs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 
 import org.ygba.youthgobudget.R;
@@ -18,5 +21,25 @@ public class DatePickerActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_picker);
         datePicker = findViewById(R.id.date_picker_activity_date_picker);
+        Button okButton = findViewById(R.id.button_ok);
+        Button cancelButton = findViewById(R.id.button_cancel);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String date_string = datePicker.getDayOfMonth() + "/" + datePicker.getMonth() + datePicker.getYear();
+                Intent intent = new Intent();
+                intent.putExtra(SELECTED_DATE, date_string);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setResult(RESULT_CANCELED, null);
+                finish();
+            }
+        });
     }
 }
