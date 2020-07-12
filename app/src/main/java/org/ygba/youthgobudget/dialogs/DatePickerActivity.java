a77188a27c845fb3b295f01b9d9d6ef44151dd81
simonojok19 <simonojok19@gmail.com>
2020-07-12 13:46:04 +0300

    added dialog activity

---
+++ app/src/main/java/org/ygba/youthgobudget/dialogs/DatePickerActivity.java
@@ -0,0 +1,16 @@
+package org.ygba.youthgobudget.dialogs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.ygba.youthgobudget.R;
+
+public class DatePickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_picker);
+    }
+}
