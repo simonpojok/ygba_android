38d7531de6cd5313534db26e032c5bd815f53711
simonojok19 <simonojok19@gmail.com>
2020-07-12 14:06:25 +0300

    added date picker

--- app/src/main/java/org/ygba/youthgobudget/dialogs/DatePickerActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/dialogs/DatePickerActivity.java
@@ -3,16 +3,19 @@ package org.ygba.youthgobudget.dialogs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.DatePicker;
 
 import org.ygba.youthgobudget.R;
 
 public class DatePickerActivity extends AppCompatActivity {
 
     public static final String SELECTED_DATE = "org.ygba.youthgobudget.dialogs.SELECTED_DATE";
+    private DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_picker);
+        datePicker = findViewById(R.id.date_picker_activity_date_picker);
     }
 }
