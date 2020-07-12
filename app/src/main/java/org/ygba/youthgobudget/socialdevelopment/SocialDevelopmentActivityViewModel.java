d6a0dff218b2f9ca50eecd24881d2494d391b58b
simonojok19 <simonojok19@gmail.com>
2020-07-12 23:44:10 +0300

    handled the execption on the view model

--- app/src/main/java/org/ygba/youthgobudget/socialdevelopment/SocialDevelopmentActivityViewModel.java
+++ app/src/main/java/org/ygba/youthgobudget/socialdevelopment/SocialDevelopmentActivityViewModel.java
@@ -4,10 +4,14 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 
 import org.ygba.youthgobudget.YGBARepository;
 import org.ygba.youthgobudget.data.YGBDatabase;
 import org.ygba.youthgobudget.data.socialdevelopment.SocialDevelopmentQuestion;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class SocialDevelopmentActivityViewModel extends AndroidViewModel {
     private YGBARepository ygbaRepository;
@@ -19,5 +23,14 @@ public class SocialDevelopmentActivityViewModel extends AndroidViewModel {
 
     public void saveSocialDevelopmentQuestion(SocialDevelopmentQuestion socialDevelopmentQuestion) {
         ygbaRepository.saveSocialDevelopmentQuestion(socialDevelopmentQuestion);
+    }
+
+    public LiveData<List<SocialDevelopmentQuestion>> getAllSocialDevelopmentQuestions() {
+        try {
+            return ygbaRepository.getAllSocialDevelopmentQuestions();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
