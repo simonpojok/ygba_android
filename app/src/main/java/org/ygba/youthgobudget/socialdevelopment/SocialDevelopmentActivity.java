a9b09fdf071e0cd59afa31b3f8b0444104bd7c1c
simonojok19 <simonojok19@gmail.com>
2020-07-12 12:13:55 +0300

    added some common views and binded them

--- app/src/main/java/org/ygba/youthgobudget/socialdevelopment/SocialDevelopmentActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/socialdevelopment/SocialDevelopmentActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.ygba.youthgobudget.R;
@@ -11,6 +13,12 @@ import org.ygba.youthgobudget.R;
 public class SocialDevelopmentActivity extends AppCompatActivity {
     private SocialDevelopmentActivityViewModel activityViewModel;
     private TextView socialDateTextView;
+    private Spinner financialYearSpinner;
+    private EditText villageTextEdit;
+    private EditText parishTextEdit;
+    private EditText divisionTextEdit;
+    private EditText fullNameTextEdit;
+    private EditText agentTelEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +31,11 @@ public class SocialDevelopmentActivity extends AppCompatActivity {
 
     private void initViews() {
         socialDateTextView = findViewById(R.id.social_date_text_view);
+        financialYearSpinner = findViewById(R.id.financial_year_spinner);
+        villageTextEdit = findViewById(R.id.village_text_edit);
+        parishTextEdit = findViewById(R.id.parish_text_edit);
+        divisionTextEdit = findViewById(R.id.division_text_edit);
+        fullNameTextEdit = findViewById(R.id.ygb_agent_name_edit_view);
+        agentTelEditText = findViewById(R.id.ygb_agent_tel_edit_view);
     }
 }
