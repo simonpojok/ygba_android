bfe3eec261c045310b1ad3fce7e246c331da5fca
simonojok19 <simonojok19@gmail.com>
2020-07-12 12:07:53 +0300

    added and binded the text view

--- app/src/main/java/org/ygba/youthgobudget/socialdevelopment/SocialDevelopmentActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/socialdevelopment/SocialDevelopmentActivity.java
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import org.ygba.youthgobudget.R;
 
 public class SocialDevelopmentActivity extends AppCompatActivity {
     private SocialDevelopmentActivityViewModel activityViewModel;
+    private TextView socialDateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_development);
-
         activityViewModel = new ViewModelProvider(this).get(SocialDevelopmentActivityViewModel.class);
+
+        initViews();
+    }
+
+    private void initViews() {
+        socialDateTextView = findViewById(R.id.social_date_text_view);
     }
 }
