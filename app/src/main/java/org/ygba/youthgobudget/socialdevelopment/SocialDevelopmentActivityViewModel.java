a50551489346978a2d725076f6c3162cb1a87e11
simonojok19 <simonojok19@gmail.com>
2020-07-12 11:50:13 +0300

    added the view model

---
+++ app/src/main/java/org/ygba/youthgobudget/socialdevelopment/SocialDevelopmentActivityViewModel.java
@@ -0,0 +1,12 @@
+package org.ygba.youthgobudget.socialdevelopment;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class SocialDevelopmentActivityViewModel extends AndroidViewModel {
+    public SocialDevelopmentActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
