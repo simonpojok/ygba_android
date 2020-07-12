80c67ac933f57391d32818ae82136d7e2c47a502
simonojok19 <simonojok19@gmail.com>
2020-07-12 22:40:27 +0300

    added row one complete

--- app/src/main/java/org/ygba/youthgobudget/data/socialdevelopment/SocialDevelopmentConstants.java
+++ app/src/main/java/org/ygba/youthgobudget/data/socialdevelopment/SocialDevelopmentConstants.java
@@ -1,4 +1,32 @@
 package org.ygba.youthgobudget.data.socialdevelopment;
 
+import androidx.room.Entity;
+
 public class SocialDevelopmentConstants {
+
+    public static final String TABLE_NAME = "social_dev";
+    public static final String PRIMARY_KEY = "id";
+    public static final String FINANCIAL_YEAR = "financial_year";
+    public static final String DATE = "date";
+    public static final String DISTRICT = "district";
+    public static final String VILLAGE = "village";
+    public static final String PARISH = "parish";
+    public static final String DIVISION = "division";
+    public static final String AGENT_NAME = "agent_name";
+    public static final String YGBA_TEL = "tel";
+    public static final String Q2_COMMUNITY_EXPECTED_APPROVED = "q2_com_expec";
+    public static final String Q2_COMMUNITY_AMOUNT_RECEIVED = "q2_com_received";
+    public static final String Q2_COMMUNITY_DATE_RECEIVED = "q2_com_date_received";
+    public static final String Q2_COMMUNITY_DATE_WITHDRAWN = "q2_com_date_withdrawn";
+    public static final String Q2_OTHER_AMOUNT_EXPECTED = "q2_others_expected";
+    public static final String Q2_OTHER_AMOUNT_RECEIVED = "q2_others_amount_received";
+    public static final String Q2_OTHERS_DATE_RECEIVED = "q2_others_date_received";
+    public static final String Q2_OTHERS_DAE_WITHDRAWN = "q2_others_date_withdrawn";
+    public static final String Q3_WOMEN_EMPOWERMENT_OBJECTIVE = "q3_w_e_objective";
+    public static final String Q3_WOMEN_EMPOWERMENT_OBJECTIVE_REASON = "q3_w_e_objective_reason";
+    public static final String Q3_WOMEN_GROUP_1_NAME = "q3_w_g_1_name";
+    public static final String Q3_WOMEN_GROUP_1_VILLAGE = "q3_w_g_1_village";
+    public static final String Q3_WOMEN_GROUP_1_MALE_NUMBER = "q3_w_g_1_m_numbers";
+    public static final String Q3_WOMEN_GROUP_1_FEMALE_NUMBER = "q3_w_g_1_f_numbers";
+    public static final String Q3_WOMEN_GROUP_1_AMOUNT_RECEIVED = "q3_w_g_1_a_recieived";
 }
--- app/src/main/java/org/ygba/youthgobudget/data/socialdevelopment/SocialDevelopmentQuestion.java
+++ app/src/main/java/org/ygba/youthgobudget/data/socialdevelopment/SocialDevelopmentQuestion.java
@@ -1,4 +1,82 @@
 package org.ygba.youthgobudget.data.socialdevelopment;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = SocialDevelopmentConstants.TABLE_NAME)
 public class SocialDevelopmentQuestion {
+
+    @ColumnInfo(name = SocialDevelopmentConstants.PRIMARY_KEY)
+    @PrimaryKey(autoGenerate = true)
+    private int primaryKey;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.FINANCIAL_YEAR)
+    private String financialYear;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.DATE)
+    private String date;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.DISTRICT)
+    private String district;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.VILLAGE)
+    private String village;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.PARISH)
+    private String parish;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.DIVISION)
+    private String division;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.AGENT_NAME)
+    private String ygbaAgentFullName;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.YGBA_TEL)
+    private String ygbaTel;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.Q2_COMMUNITY_EXPECTED_APPROVED)
+    private String q2CommunityExpected;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.Q2_COMMUNITY_AMOUNT_RECEIVED)
+    private String q2CommunityAmountReceived;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.Q2_COMMUNITY_DATE_RECEIVED)
+    private String q2CommunityDateReceived;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.Q2_COMMUNITY_DATE_WITHDRAWN)
+    private String q2CommunityDateWithdrawn;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.Q2_OTHER_AMOUNT_EXPECTED)
+    private String q2OtherExpectedAmount;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.Q2_OTHER_AMOUNT_RECEIVED)
+    private String q2OtherAmountedReceived;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.Q2_OTHERS_DATE_RECEIVED)
+    private String q2OthersDateReceived;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.Q2_OTHERS_DAE_WITHDRAWN)
+    private String q2OthersDateWithdrawn;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.Q3_WOMEN_EMPOWERMENT_OBJECTIVE)
+    private String q3WomenEmpowermentObjective;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.Q3_WOMEN_EMPOWERMENT_OBJECTIVE_REASON)
+    private String q3WomenEmpowermentObjectiveReason;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.Q3_WOMEN_GROUP_1_NAME)
+    private String q3WomanGroup1Name;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.Q3_WOMEN_GROUP_1_VILLAGE)
+    private String q3WomenGroup1Village;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.Q3_WOMEN_GROUP_1_MALE_NUMBER)
+    private String q3WomenGroup1MaleNumber;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.Q3_WOMEN_GROUP_1_FEMALE_NUMBER)
+    private String q3WomenGroup1FemaleNumber;
+
+    @ColumnInfo(name = SocialDevelopmentConstants.Q3_WOMEN_GROUP_1_AMOUNT_RECEIVED)
+    private String q3WomenGroup1AmountReceived;
 }
