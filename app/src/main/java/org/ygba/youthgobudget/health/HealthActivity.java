a32acada1ddf3797a7436d25dadfc8ef6299e457
simonojok19 <simonojok19@gmail.com>
2020-07-13 13:39:24 +0300

    added and binding the common feilds

--- app/src/main/java/org/ygba/youthgobudget/health/HealthActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/health/HealthActivity.java
@@ -3,14 +3,36 @@ package org.ygba.youthgobudget.health;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import org.ygba.youthgobudget.R;
 
 public class HealthActivity extends AppCompatActivity {
+    TextView hDateTextView;
+    Spinner hFinancialSpinner;
+    EditText villageEditText;
+    EditText parishEditText;
+    EditText divisionEditText;
+    EditText agentFulNameEditText;
+    EditText agentTelEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health);
+
+        initViews();
+    }
+
+    private void initViews() {
+        hDateTextView = findViewById(R.id.date_text_view);
+        hFinancialSpinner = findViewById(R.id.financial_year_spinner);
+        villageEditText = findViewById(R.id.village_text_edit);
+        parishEditText = findViewById(R.id.parish_text_edit);
+        divisionEditText = findViewById(R.id.division_text_edit);
+        agentFulNameEditText = findViewById(R.id.ygb_agent_name_edit_view);
+        agentTelEditText = findViewById(R.id.ygb_agent_tel_edit_view);
     }
 }
