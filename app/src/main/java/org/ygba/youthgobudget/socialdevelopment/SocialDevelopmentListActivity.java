158d4fe8fcf1121dfad90e4f1a681e4f3ee63d35
simonojok19 <simonojok19@gmail.com>
2020-07-13 13:13:23 +0300

    added another activity

---
+++ app/src/main/java/org/ygba/youthgobudget/socialdevelopment/SocialDevelopmentListActivity.java
@@ -0,0 +1,16 @@
+package org.ygba.youthgobudget.socialdevelopment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.ygba.youthgobudget.R;
+
+public class SocialDevelopmentListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_development_list);
+    }
+}
