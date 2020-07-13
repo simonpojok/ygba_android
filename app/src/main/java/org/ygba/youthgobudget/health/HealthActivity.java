b2cfe01ec5e27e0c7737b593f8d32380d429c11f
simonojok19 <simonojok19@gmail.com>
2020-07-13 14:12:16 +0300

    added some attributeds

--- app/src/main/java/org/ygba/youthgobudget/health/HealthActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/health/HealthActivity.java
@@ -1,8 +1,11 @@
 package org.ygba.youthgobudget.health;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -20,6 +23,9 @@ public class HealthActivity extends AppCompatActivity {
     EditText qQANameAndGradeHealthCenterEditText;
     EditText hQBAttendanceEditText;
     EditText hQCInpatientNumberEditText;
+    EditText hQ1RecurrentApprovedEditText;
+    EditText hQ1RecurrentBudgetReleaseTextEdit;
+    EditText hQ1RecurrentDateReceivedEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +33,19 @@ public class HealthActivity extends AppCompatActivity {
         setContentView(R.layout.activity_health);
 
         initViews();
+        initEventHandlers();
     }
 
+    private void initEventHandlers() {
+        hQ1RecurrentDateReceivedEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private void initViews() {
         hDateTextView = findViewById(R.id.date_text_view);
         hFinancialSpinner = findViewById(R.id.financial_year_spinner);
@@ -40,5 +57,11 @@ public class HealthActivity extends AppCompatActivity {
         qQANameAndGradeHealthCenterEditText = findViewById(R.id.h_name_and_grade_health_center);
         hQBAttendanceEditText = findViewById(R.id.h_question_d_attendance_edit_text);
         hQCInpatientNumberEditText = findViewById(R.id.h_question_c_inpatient_number);
+        hQ1RecurrentApprovedEditText = findViewById(R.id.h_question_1_recurrent_approved);
+        hQ1RecurrentBudgetReleaseTextEdit = findViewById(R.id.h_question_1_recurrent_budget_released_edit_text);
+        hQ1RecurrentDateReceivedEditText = findViewById(R.id.h_question_1_recurrent_date_received_edit_text);
+        hQ1RecurrentDateReceivedEditText.setInputType(View.AUTOFILL_TYPE_NONE);
     }
+
+
 }
