150939926441ec4bb24a91b2ead3137a97ba1575
simonojok19 <somonojok19@gmail.com>
2020-07-15 12:27:32 +0300

    wired up the text view with the current date and others

--- app/src/main/java/org/ygba/youthgobudget/water_and_environment/WaterEnvironmentActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/water_and_environment/WaterEnvironmentActivity.java
@@ -3,14 +3,27 @@ package org.ygba.youthgobudget.water_and_environment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import org.ygba.youthgobudget.R;
+import org.ygba.youthgobudget.utils.DynamicData;
 
 public class WaterEnvironmentActivity extends AppCompatActivity {
+    private TextView waterDateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_water_environment);
+        initViews();
+        populateViews();
+    }
+
+    private void initViews() {
+        waterDateTextView = findViewById(R.id.water_date_text_view);
+    }
+
+    private void populateViews() {
+        waterDateTextView.setText(DynamicData.getDate());
     }
 }
