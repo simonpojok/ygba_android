2ecb1417bf9a8e2ce34ab34f7e620daab60950d5
simonojok19 <somonojok19@gmail.com>
2020-07-15 13:51:56 +0300

    added question 5 introduction@

--- app/src/main/java/org/ygba/youthgobudget/water_and_environment/WaterEnvironmentActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/water_and_environment/WaterEnvironmentActivity.java
@@ -23,6 +23,7 @@ public class WaterEnvironmentActivity extends AppCompatActivity {
     private EditText wAgentNumberET;
     private EditText wQ1ObjectiveReasonET;
     private EditText wQ2EditText;
+    private EditText wQ4EditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +45,7 @@ public class WaterEnvironmentActivity extends AppCompatActivity {
         wAgentNumberET = findViewById(R.id.ygb_agent_no_edit_view);
         wQ1ObjectiveReasonET = findViewById(R.id.water_question_1_if_no);
         wQ2EditText = findViewById(R.id.water_question_2_edit_text);
+        wQ4EditText = findViewById(R.id.water_question_4_edit_text);
     }
 
     private void populateViews() {
