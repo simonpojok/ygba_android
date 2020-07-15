4294529b4acc5fc931a9873640476bc98a8fd83a
simonojok19 <somonojok19@gmail.com>
2020-07-15 21:10:43 +0300

    added financial year spinner

--- app/src/main/java/org/ygba/youthgobudget/budget_information/BudgetInformationActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/budget_information/BudgetInformationActivity.java
@@ -3,14 +3,22 @@ package org.ygba.youthgobudget.budget_information;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 import org.ygba.youthgobudget.R;
 
 public class BudgetInformationActivity extends AppCompatActivity {
+    private Spinner financialYearSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_information);
+
+        initViews();
+    }
+
+    private void initViews() {
+        financialYearSpinner = findViewById(R.id.financial_year_spinner);
     }
 }
