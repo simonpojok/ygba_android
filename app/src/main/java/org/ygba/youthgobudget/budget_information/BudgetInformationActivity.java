8b538a409191c658e04762d67983ae8d3e542d1b
simonojok19 <somonojok19@gmail.com>
2020-07-15 21:22:50 +0300

    finished column one

--- app/src/main/java/org/ygba/youthgobudget/budget_information/BudgetInformationActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/budget_information/BudgetInformationActivity.java
@@ -3,12 +3,14 @@ package org.ygba.youthgobudget.budget_information;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import org.ygba.youthgobudget.R;
 
 public class BudgetInformationActivity extends AppCompatActivity {
     private Spinner financialYearSpinner;
+    private EditText administrationApprovedBudgetED;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +22,6 @@ public class BudgetInformationActivity extends AppCompatActivity {
 
     private void initViews() {
         financialYearSpinner = findViewById(R.id.financial_year_spinner);
+        administrationApprovedBudgetED = findViewById(R.id.approved_budget_administration);
     }
 }
