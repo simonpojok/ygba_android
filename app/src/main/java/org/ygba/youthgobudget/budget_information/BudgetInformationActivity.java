d89d73dd2054f19136e4cf71e4b71e99037b196e
simonojok19 <somonojok19@gmail.com>
2020-07-15 21:38:13 +0300

    added another row

--- app/src/main/java/org/ygba/youthgobudget/budget_information/BudgetInformationActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/budget_information/BudgetInformationActivity.java
@@ -12,6 +12,8 @@ public class BudgetInformationActivity extends AppCompatActivity {
     private Spinner financialYearSpinner;
     private EditText administrationApprovedBudgetED;
     private EditText administrationPercentageED;
+    private EditText financeApprovedBudgetED;
+    private EditText financePercentage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +27,7 @@ public class BudgetInformationActivity extends AppCompatActivity {
         financialYearSpinner = findViewById(R.id.financial_year_spinner);
         administrationApprovedBudgetED = findViewById(R.id.approved_budget_administration);
         administrationPercentageED = findViewById(R.id.percentage_administration);
+        financeApprovedBudgetED = findViewById(R.id.approved_budget_finance);
+        financePercentage = findViewById(R.id.percentage_finance);
     }
 }
