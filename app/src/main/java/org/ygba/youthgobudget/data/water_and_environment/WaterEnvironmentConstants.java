c33c5f8b803e4d1687cc0a026336434712d0f495
simonojok19 <somonojok19@gmail.com>
2020-07-15 15:59:54 +0300

    added some fileds@

--- app/src/main/java/org/ygba/youthgobudget/data/water_and_environment/WaterEnvironmentConstants.java
+++ app/src/main/java/org/ygba/youthgobudget/data/water_and_environment/WaterEnvironmentConstants.java
@@ -1,4 +1,24 @@
 package org.ygba.youthgobudget.data.water_and_environment;
 
 public class WaterEnvironmentConstants {
+    public static final String TABLE_NAME = "water_and_env";
+    public static final String PRIMARY_KEY = "id";
+    public static final String FINANCIAL_YEAR = "financial_year";
+    public static final String DATE = "date";
+    public static final String DISTRICT = "district";
+    public static final String VILLAGE = "village";
+    public static final String PARISH = "parish";
+    public static final String SUB_COUNTY = "sub_county";
+    public static final String YGB_AGENT = "agent_name";
+    public static final String AGENT_TEL = "agent_tel";
+    public static final String QUESTION_1_OBJECTIVE_ANSWER = "q_1_objective";
+    public static final String QUESTION_1_OBJECTIVE_REASON = "q_1_reason";
+    public static final String QUESTION_2_SANITATION = "q_2_reason";
+    public static final String QUESTION_3_OBJECTIVE = "q_3_objective";
+    public static final String QUESTION_5_LONG_TEXT = "q_5_text";
+    public static final String SUB_COUNTY_1 = "q_5_sub_1";
+    public static final String QUESTION_5_WATER_SOURCE_1 = "q_5_ws_1";
+    public static final String QUESTION_5_FUNCTIONAL_1 = "q_5_functional_1";
+    public static final String QUESTION_5_NONE_FUNCTIONAL_1 = "q_5_none_functional_1";
+    public static final String QUESTION_5_NO_WATER_SOURCE_AVAILABLE_1 = "q_5_no_ws_available_1";
 }
--- app/src/main/java/org/ygba/youthgobudget/data/water_and_environment/WaterEnvironmentQuestion.java
+++ app/src/main/java/org/ygba/youthgobudget/data/water_and_environment/WaterEnvironmentQuestion.java
@@ -1,4 +1,66 @@
 package org.ygba.youthgobudget.data.water_and_environment;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = WaterEnvironmentConstants.TABLE_NAME)
 public class WaterEnvironmentQuestion {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = WaterEnvironmentConstants.PRIMARY_KEY)
+    private int primaryKey;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.FINANCIAL_YEAR)
+    private String financialYear;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.DATE)
+    private String date;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.DISTRICT)
+    private String district;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.VILLAGE)
+    private String village;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.PARISH)
+    private String parish;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.SUB_COUNTY)
+    private String sub_county;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.YGB_AGENT)
+    private String agentName;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.AGENT_TEL)
+    private String agentTel;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.QUESTION_1_OBJECTIVE_ANSWER)
+    private boolean question1Objective;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.QUESTION_1_OBJECTIVE_REASON)
+    private String question1ObjectiveReason;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.QUESTION_2_SANITATION)
+    private String question2Water;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.QUESTION_3_OBJECTIVE)
+    private String question3Objective;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.QUESTION_5_LONG_TEXT)
+    private String question5Text;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.SUB_COUNTY_1)
+    private String subCounty1;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.QUESTION_5_WATER_SOURCE_1)
+    private String waterSource1;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.QUESTION_5_FUNCTIONAL_1)
+    private int question5Functional1;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.QUESTION_5_NONE_FUNCTIONAL_1)
+    private int question5NoneFunctional1;
+
+    @ColumnInfo(name = WaterEnvironmentConstants.QUESTION_5_NO_WATER_SOURCE_AVAILABLE_1)
+    private int question5WaterSoureAvailable1;
 }
