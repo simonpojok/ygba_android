d072f6f7b3f3c137e57023419316db40047981bc
simonojok19 <somonojok19@gmail.com>
2020-07-15 12:19:08 +0300

    started working on the water and environment activity and logic

---
+++ app/src/main/java/org/ygba/youthgobudget/water_and_environment/WaterEnvironmentActivity.java
@@ -0,0 +1,16 @@
+package org.ygba.youthgobudget.water_and_environment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.ygba.youthgobudget.R;
+
+public class WaterEnvironmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_environment);
+    }
+}
---
+++ app/src/main/java/org/ygba/youthgobudget/water_and_environment/WaterEnvironmentViewModel.java
@@ -0,0 +1,4 @@
+package org.ygba.youthgobudget.water_and_environment;
+
+public class WaterEnvironmentViewModel {
+}
