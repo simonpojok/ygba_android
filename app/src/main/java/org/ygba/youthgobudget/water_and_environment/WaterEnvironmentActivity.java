540b56c2341ef97cfbde33cfe9ea18f82dba057b
simonojok19 <somonojok19@gmail.com>
2020-07-15 12:39:20 +0300

    adding some basic data to the view

--- app/src/main/java/org/ygba/youthgobudget/water_and_environment/WaterEnvironmentActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/water_and_environment/WaterEnvironmentActivity.java
@@ -3,6 +3,8 @@ package org.ygba.youthgobudget.water_and_environment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.ygba.youthgobudget.R;
@@ -10,6 +12,11 @@ import org.ygba.youthgobudget.utils.DynamicData;
 
 public class WaterEnvironmentActivity extends AppCompatActivity {
     private TextView waterDateTextView;
+    private Spinner waterFinancialYearSpinner;
+    private EditText waterDistrictEditText;
+    private EditText waterVillageEditText;
+    private EditText wParishEditText;
+    private EditText wDivisionET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +28,15 @@ public class WaterEnvironmentActivity extends AppCompatActivity {
 
     private void initViews() {
         waterDateTextView = findViewById(R.id.water_date_text_view);
+        waterFinancialYearSpinner = findViewById(R.id.water_financial_year_spinner);
+        waterVillageEditText = findViewById(R.id.water_village_text_edit);
+        waterDistrictEditText = findViewById(R.id.water_district_text_edit);
+        wParishEditText = findViewById(R.id.water_parish_text_edit);
+        wDivisionET = findViewById(R.id.water_division_text_edit);
     }
 
     private void populateViews() {
         waterDateTextView.setText(DynamicData.getDate());
+        // init spinner
     }
 }
