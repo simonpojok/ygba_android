7762a34b4b0abe0bf195fc8c96aae21bfb76e78c
simonojok19 <simonojok19@gmail.com>
2020-07-16 08:40:45 +0300

    added admninstrations

--- app/src/main/java/org/ygba/youthgobudget/data/budget_information/BudgetInformationForm.java
+++ app/src/main/java/org/ygba/youthgobudget/data/budget_information/BudgetInformationForm.java
@@ -1,4 +1,21 @@
 package org.ygba.youthgobudget.data.budget_information;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = BudgetInformationFormConstants.TABLE_NAME)
 public class BudgetInformationForm {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = BudgetInformationFormConstants.PRIMARY_KEY)
+    private int primaryKey;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.FINANCIAL_YEAR)
+    private String financialYear;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.ADMINISTRATION_APPROVED_BUDGET)
+    private int adminApprovedBudget;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.ADMINISTRATION_PERCENTAGE)
+    private int adminPercentage;
 }
--- app/src/main/java/org/ygba/youthgobudget/data/budget_information/BudgetInformationFormConstants.java
+++ app/src/main/java/org/ygba/youthgobudget/data/budget_information/BudgetInformationFormConstants.java
@@ -1,4 +1,9 @@
 package org.ygba.youthgobudget.data.budget_information;
 
 public class BudgetInformationFormConstants {
+    public static final String TABLE_NAME = "budget_information";
+    public static final String PRIMARY_KEY = "primary_key";
+    public static final String FINANCIAL_YEAR = "fin_year";
+    public static final String ADMINISTRATION_APPROVED_BUDGET = "admin_approved";
+    public static final String ADMINISTRATION_PERCENTAGE = "admin_percentage";
 }
