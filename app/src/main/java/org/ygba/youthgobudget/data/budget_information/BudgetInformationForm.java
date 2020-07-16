ec4e37cf47612648bb89268b08ddc78e025cd2df
simonojok19 <simonojok19@gmail.com>
2020-07-16 08:52:06 +0300

    added some form content

--- app/src/main/java/org/ygba/youthgobudget/data/budget_information/BudgetInformationForm.java
+++ app/src/main/java/org/ygba/youthgobudget/data/budget_information/BudgetInformationForm.java
@@ -18,4 +18,52 @@ public class BudgetInformationForm {
 
     @ColumnInfo(name = BudgetInformationFormConstants.ADMINISTRATION_PERCENTAGE)
     private int adminPercentage;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.FINANCE_APPROVED_BUDGET)
+    private int financeApprovedBudget;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.FINANCE_PERCENTAGE)
+    private int financePercentage;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.STATUTORY_BODIES_APPROVED_BUDGET)
+    private int statutoryBodiesApprovedBudget;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.STATUTORY_BODIES_PERCENTAGE)
+    private int statutoryBodiesPercentage;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.PRODUCTION_APPROVED_BUDGET)
+    private int productionApprovedBudget;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.PRODUCTION_PERCENTAGE)
+    private int productionPercentage;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.HEALTH_APPROVED_BUDGET)
+    private int healthApprovedBudget;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.HEALTH_PERCENTAGE)
+    private int healthPercentage;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.EDUCATION_APPROVED_BUDGET)
+    private int educationApprovedBudget;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.EDUCATION_PERCENTAGE)
+    private int educationPercentage;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.ROADS_ENGINEERING_APPROVED_BUDGET)
+    private int roadEngineeringApprovedBudget;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.ROADS_ENGINEERING_PERCENTAGE)
+    private int roadEngineeringPercentage;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.WATER_APPROVED_BUDGET)
+    private int waterApprovedBudget;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.WATER_PERCENTAGE)
+    private int waterPercentage;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.NATUREAL_RESOURCES_APPROVED_BUDGET)
+    private int naturalApprovedBudget;
+
+    @ColumnInfo(name = BudgetInformationFormConstants.NATUREAL_RESOURCES_PERCENTAGE)
+    private int naturalPercentage;
 }
--- app/src/main/java/org/ygba/youthgobudget/data/budget_information/BudgetInformationFormConstants.java
+++ app/src/main/java/org/ygba/youthgobudget/data/budget_information/BudgetInformationFormConstants.java
@@ -6,4 +6,20 @@ public class BudgetInformationFormConstants {
     public static final String FINANCIAL_YEAR = "fin_year";
     public static final String ADMINISTRATION_APPROVED_BUDGET = "admin_approved";
     public static final String ADMINISTRATION_PERCENTAGE = "admin_percentage";
+    public static final String FINANCE_APPROVED_BUDGET = "fin_approved";
+    public static final String FINANCE_PERCENTAGE = "fin_percentage";
+    public static final String STATUTORY_BODIES_APPROVED_BUDGET = "statu_approved";
+    public static final String STATUTORY_BODIES_PERCENTAGE = "statu_per";
+    public static final String PRODUCTION_APPROVED_BUDGET = "pro_approved";
+    public static final String PRODUCTION_PERCENTAGE = "pro_perc";
+    public static final String HEALTH_APPROVED_BUDGET = "health_approved";
+    public static final String HEALTH_PERCENTAGE = "health_percentage";
+    public static final String EDUCATION_APPROVED_BUDGET = "education_approved";
+    public static final String EDUCATION_PERCENTAGE = "education_percentage";
+    public static final String ROADS_ENGINEERING_APPROVED_BUDGET = "road_approved";
+    public static final String ROADS_ENGINEERING_PERCENTAGE = "road_percentagae";
+    public static final String WATER_APPROVED_BUDGET = "water_approved";
+    public static final String WATER_PERCENTAGE = "water_approved";
+    public static final String NATUREAL_RESOURCES_APPROVED_BUDGET = "natural_approved";
+    public static final String NATUREAL_RESOURCES_PERCENTAGE = "natural_percentage";
 }
