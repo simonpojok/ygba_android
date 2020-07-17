e58de5c441bd827a4a34bddfb3e0551667188e29
simonojok19 <simonojok19@gmail.com>
2020-07-17 14:57:42 +0300

    organizing the files

--- app/src/main/java/org/ygba/youthgobudget/data/education/EducationQuestion.java
+++ app/src/main/java/org/ygba/youthgobudget/data/education/EducationQuestion.java
@@ -2,9 +2,25 @@ package org.ygba.youthgobudget.data.education;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = EducationQuestionConstants.TABLE_NAME)
 public class EducationQuestion {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = EducationQuestionConstants.PRIMARY_KEY)
+    private int id;
 
+
+
+    public static class Builder {
+        private final EducationQuestion INSTANCE;
+        public Builder() {
+            INSTANCE = new EducationQuestion();
+        }
+    }
+
+    @Ignore
+    private EducationQuestion() {
+    }
 }
--- app/src/main/java/org/ygba/youthgobudget/data/education/EducationQuestionConstants.java
+++ app/src/main/java/org/ygba/youthgobudget/data/education/EducationQuestionConstants.java
@@ -2,4 +2,5 @@ package org.ygba.youthgobudget.data.education;
 
 public class EducationQuestionConstants {
     public static final String TABLE_NAME = "educ_table";
+    public static final String PRIMARY_KEY = "primary_key";
 }
