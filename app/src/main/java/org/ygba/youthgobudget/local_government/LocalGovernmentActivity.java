4169c5c626574e85b2169a3b7c17d63271611880
simonojok19 <simonojok19@gmail.com>
2020-07-17 12:09:15 +0300

    added local government activity

---
+++ app/src/main/java/org/ygba/youthgobudget/local_government/LocalGovernmentActivity.java
@@ -0,0 +1,16 @@
+package org.ygba.youthgobudget.local_government;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.ygba.youthgobudget.R;
+
+public class LocalGovernmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_government);
+    }
+}
