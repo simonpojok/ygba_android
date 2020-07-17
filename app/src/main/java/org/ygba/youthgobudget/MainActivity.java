c44b29df5db590ad11167cd4c19fa526d6970421
simonojok19 <simonojok19@gmail.com>
2020-07-17 12:37:24 +0300

    removed the side bar

--- app/src/main/java/org/ygba/youthgobudget/MainActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/MainActivity.java
@@ -1,9 +1,12 @@
 package org.ygba.youthgobudget;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
