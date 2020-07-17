5276bf014c8bb7c37f1146332cb8c21a91253f9f
simonojok19 <simonojok19@gmail.com>
2020-07-17 09:49:56 +0300

    added the first worker

---
+++ app/src/main/java/org/ygba/youthgobudget/agriculture/AgricultureUploadWorker.java
@@ -0,0 +1,19 @@
+package org.ygba.youthgobudget.agriculture;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class AgricultureUploadWorker extends Worker {
+    public AgricultureUploadWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        return null;
+    }
+}
