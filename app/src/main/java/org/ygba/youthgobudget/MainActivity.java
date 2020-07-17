d9f6885742835d87ba027639607d27567ffde5a7
simonojok19 <simonojok19@gmail.com>
2020-07-17 12:56:22 +0300

    adding more activities

--- app/src/main/java/org/ygba/youthgobudget/MainActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/MainActivity.java
@@ -11,6 +11,12 @@ import android.view.MenuItem;
 import android.view.View;
 
 import org.ygba.youthgobudget.agriculture.AgricultureActivity;
+import org.ygba.youthgobudget.budget_cycle.BudgetCycleActivity;
+import org.ygba.youthgobudget.health.HealthActivity;
+import org.ygba.youthgobudget.local_government.LocalGovernmentActivity;
+import org.ygba.youthgobudget.polls.PollActivity;
+import org.ygba.youthgobudget.social_development.SocialDevelopmentActivity;
+import org.ygba.youthgobudget.water_and_environment.WaterEnvironmentActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +31,55 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, AgricultureActivity.class));
+            }
+        });
+
+        findViewById(R.id.cv_allocation_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        findViewById(R.id.cv_social_development_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SocialDevelopmentActivity.class));
+            }
+        });
+
+        findViewById(R.id.water_environment_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, WaterEnvironmentActivity.class));
+            }
+        });
+
+        findViewById(R.id.cv_local_government_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LocalGovernmentActivity.class));
+            }
+        });
+
+        findViewById(R.id.cv_budget_cycle_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, BudgetCycleActivity.class));
+            }
+        });
+
+        findViewById(R.id.cv_health_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, HealthActivity.class));
+            }
+        });
+
+        findViewById(R.id.cv_polls_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, PollActivity.class));
             }
         });
 
---
+++ app/src/main/java/org/ygba/youthgobudget/polls/PollActivity.java
@@ -0,0 +1,16 @@
+package org.ygba.youthgobudget.polls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.ygba.youthgobudget.R;
+
+public class PollActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poll);
+    }
+}
