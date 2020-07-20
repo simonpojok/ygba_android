a4fa79e47b923c887e4b1dfc68433a186a3c0061
simonojok19 <simonojok19@gmail.com>
2020-07-20 17:41:07 +0300

    added community wishes activity

---
+++ app/src/main/java/org/ygba/youthgobudget/community_wishes/CommunityWishesActivity.java
@@ -0,0 +1,16 @@
+package org.ygba.youthgobudget.community_wishes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.ygba.youthgobudget.R;
+
+public class CommunityWishesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community_wishes);
+    }
+}
