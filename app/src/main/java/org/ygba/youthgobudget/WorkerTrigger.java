aae089f3f4503ec8a10fd7eea857665f91259d50
simonojok19 <simonojok19@gmail.com>
2020-08-01 00:21:40 +0300

    added worker trigger

--- app/src/main/java/org/ygba/youthgobudget/WorkerTrigger.java
+++ app/src/main/java/org/ygba/youthgobudget/WorkerTrigger.java
@@ -1,4 +1,12 @@
 package org.ygba.youthgobudget;
 
 public class WorkerTrigger {
+
+    public static void startAllUploadWorker() {
+        startAgricultureUploadWorker();
+    }
+
+    private static void startAgricultureUploadWorker() {
+
+    }
 }
