81f21162b6937b042aea386f5c2d9bde3ec00c08
simonojok19 <simonojok19@gmail.com>
2020-08-01 00:30:12 +0300

    added trigger complete

--- app/src/main/java/org/ygba/youthgobudget/WorkerTrigger.java
+++ app/src/main/java/org/ygba/youthgobudget/WorkerTrigger.java
@@ -1,12 +1,33 @@
 package org.ygba.youthgobudget;
 
+import android.content.Context;
+
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+
+import org.ygba.youthgobudget.agriculture.AgricultureUploadWorker;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.ygba.youthgobudget.utils.Constants.WORKER_TIME_INTERVAL_MINUTES;
+
 public class WorkerTrigger {
 
-    public static void startAllUploadWorker() {
-        startAgricultureUploadWorker();
+    public static void startAllUploadWorker(Context context) {
+        startAgricultureUploadWorker(context);
     }
 
-    private static void startAgricultureUploadWorker() {
-
+    private static void startAgricultureUploadWorker(Context context) {
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build();
+        PeriodicWorkRequest workRequest = new PeriodicWorkRequest.Builder(
+                AgricultureUploadWorker.class,
+                WORKER_TIME_INTERVAL_MINUTES,
+                TimeUnit.MINUTES
+        ).setConstraints(constraints).build();
+        WorkManager.getInstance(context).enqueue(workRequest);
     }
 }
--- app/src/main/java/org/ygba/youthgobudget/utils/Constants.java
+++ app/src/main/java/org/ygba/youthgobudget/utils/Constants.java
@@ -1,6 +1,8 @@
 package org.ygba.youthgobudget.utils;
+import java.util.concurrent.TimeUnit;
 
 public class Constants {
+    public static final int WORKER_TIME_INTERVAL_MINUTES = 5;
     public static final String  BASE_URL = "http://167.179.92.196/api/";
     public static final String AGRICULTURE_COLLECTION = BASE_URL + "collect/agric";
 }
