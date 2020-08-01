83b07e646536e240e7699133b2752b18c4ea23d2
simonojok19 <simonojok19@gmail.com>
2020-08-01 11:42:44 +0300

    adding worker for health

--- app/src/main/java/org/ygba/youthgobudget/water_and_environment/WaterEnvironmentUploadWorker.java
+++ app/src/main/java/org/ygba/youthgobudget/water_and_environment/WaterEnvironmentUploadWorker.java
@@ -1,4 +1,19 @@
 package org.ygba.youthgobudget.water_and_environment;
 
-public class WaterEnvironmentUploadWorker {
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class WaterEnvironmentUploadWorker extends Worker {
+    public WaterEnvironmentUploadWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        return null;
+    }
 }
