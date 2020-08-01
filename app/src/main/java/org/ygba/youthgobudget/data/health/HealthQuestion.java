22b60c57d65dda1ba901bdd6ca5bf94097d25fb2
simonojok19 <simonojok19@gmail.com>
2020-08-01 11:55:19 +0300

    adding health question

--- app/src/main/java/org/ygba/youthgobudget/data/health/HealthQuestion.java
+++ app/src/main/java/org/ygba/youthgobudget/data/health/HealthQuestion.java
@@ -1,5 +1,25 @@
 package org.ygba.youthgobudget.data.health;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
+@Entity(tableName = HealthQuestionConstants.TABLE_NAME)
 public class HealthQuestion {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = HealthQuestionConstants.PRIMARY_KEY)
+    private int primaryKey;
+
+    @ColumnInfo(name = HealthQuestionConstants.FINANCIAL_YEAR)
+    private String financialYear;
+
+    @ColumnInfo(name = HealthQuestionConstants.FINANCIAL_QUARTER)
+    private String financialYearQuarter;
+
+    @ColumnInfo(name = HealthQuestionConstants.DATE)
+    private String data;
+
+
+    private HealthQuestion() {}
 }
--- app/src/main/java/org/ygba/youthgobudget/data/health/HealthQuestionConstants.java
+++ app/src/main/java/org/ygba/youthgobudget/data/health/HealthQuestionConstants.java
@@ -1,4 +1,9 @@
 package org.ygba.youthgobudget.data.health;
 
 public class HealthQuestionConstants {
+    public static final String TABLE_NAME = "health_table";
+    public static final String PRIMARY_KEY = "primary_key";
+    public static final String FINANCIAL_YEAR = "financial_year";
+    public static final String FINANCIAL_QUARTER = "quarter";
+    public static final String DATE = "date";
 }
