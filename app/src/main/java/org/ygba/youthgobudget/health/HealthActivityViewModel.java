dbc4ec5d4aeba26ceecf661571e42cb1bd9fa107
simonojok19 <simonojok19@gmail.com>
2020-08-01 11:47:08 +0300

    added view model

---
+++ app/src/main/java/org/ygba/youthgobudget/health/HealthActivityViewModel.java
@@ -0,0 +1,12 @@
+package org.ygba.youthgobudget.health;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class HealthActivityViewModel extends AndroidViewModel {
+    public HealthActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
