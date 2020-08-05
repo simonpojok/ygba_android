c99f7834c271ddef79a002369b37547692827816
simonojok19 <simonojok19@gmail.com>
2020-08-05 18:24:31 +0300

    fixing ome errors

--- app/src/main/java/org/ygba/youthgobudget/utils/Constants.java
+++ app/src/main/java/org/ygba/youthgobudget/utils/Constants.java
@@ -3,6 +3,6 @@ import java.util.concurrent.TimeUnit;
 
 public class Constants {
     public static final int WORKER_TIME_INTERVAL_MINUTES = 5;
-    public static final String  BASE_URL = "http://167.179.92.196/api/";
+    public static final String  BASE_URL = "https://ygba.org/api/";
     public static final String AGRICULTURE_COLLECTION = BASE_URL + "collect/agric";
 }
