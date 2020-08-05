bc90395c6b2452a66979eae800cee2e5dd5fe20f
simonojok19 <simonojok19@gmail.com>
2020-08-05 20:04:45 +0300

    adding water questions

--- app/src/main/java/org/ygba/youthgobudget/water_and_environment/WaterEnvironmentUploadWorker.java
+++ app/src/main/java/org/ygba/youthgobudget/water_and_environment/WaterEnvironmentUploadWorker.java
@@ -6,6 +6,8 @@ import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.ygba.youthgobudget.YGBARepository;
 import org.ygba.youthgobudget.data.YGBDatabase;
 import org.ygba.youthgobudget.data.water_and_environment.WaterEnvironmentQuestion;
@@ -27,8 +29,16 @@ public class WaterEnvironmentUploadWorker extends Worker {
     @Override
     public Result doWork() {
         List<WaterEnvironmentQuestion> waterEnvironmentQuestions = getList();
-        for (WaterEnvironmentQuestion waterEnvironmentQuestion: waterEnvironmentQuestions) {
 
+        if (waterEnvironmentQuestions != null ) {
+            try {
+                for (WaterEnvironmentQuestion waterEnvironmentQuestion: waterEnvironmentQuestions) {
+                    JSONObject body = new JSONObject();
+                    body.put("record_id", waterEnvironmentQuestion.getPrimaryKey());
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
         return Result.success();
     }
@@ -42,3 +52,25 @@ public class WaterEnvironmentUploadWorker extends Worker {
         return null;
     }
 }
+
+        $table->string('financial_year')->nullable();
+        $table->string('Quarter')->nullable();
+        $table->date('Date')->nullable();
+        $table->string('District')->nullable();
+        $table->string('subcounty')->nullable();
+        $table->string('Parish')->nullable();
+        $table->string('Village')->nullable();
+        $table->string('Name_of_Monitor_YGB')->nullable();
+        $table->string('phone_number')->nullable();
+        $table->string('is_there_water_at_office')->nullable();
+        $table->text('If_no_water_why')->nullable();
+        $table->text('what_are_the_water_protection_activities_taking')->nullable();
+        $table->string('Are_there_rural_safe_low_Wells_boreholes')->nullable();
+        $table->text('clean_water_accessibility')->nullable();
+        $table->string('do_the_rural_water_point_sources_have_committees')->nullable();
+        $table->text('if_they_dont_have_why')->nullable();
+        $table->text('Are_there_any_wetlands_demarcated_or_protected')->nullable();
+        $table->text('areas_with_wetland_degradation')->nullable();
+        $table->text('What_are_the_tree_pl_ms_known_in_the_area')->nullable();
+        $table->timestamps();
+        $table->softDeletes();
