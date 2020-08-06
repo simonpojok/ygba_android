35e4ba226b1eb3c50e56206e6c191d809c6016bd
simonojok19 <simonojok19@gmail.com>
2020-08-06 19:44:06 +0300

    added subcounty

--- app/src/main/java/org/ygba/youthgobudget/data/helpers/sub_county/SubCountyDao.java
+++ app/src/main/java/org/ygba/youthgobudget/data/helpers/sub_county/SubCountyDao.java
@@ -1,4 +1,17 @@
 package org.ygba.youthgobudget.data.helpers.sub_county;
 
-public class SubCountyDao {
+import androidx.room.ColumnInfo;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface SubCountyDao {
+    @Insert
+    void saveSubCounty(SubCounty subCounty);
+
+    @Query("SELECT * FROM " + SubCountyConstants.TABLE_NAME)
+    List<SubCounty> getAllSubCounty();
 }
