fdee4262f8df555a1ddb4d7a28d5ada3d59c6f32
simonojok19 <simonojok19@gmail.com>
2020-08-06 19:30:34 +0300

    added get all districts

--- app/src/main/java/org/ygba/youthgobudget/data/helpers/district/DistrictDao.java
+++ app/src/main/java/org/ygba/youthgobudget/data/helpers/district/DistrictDao.java
@@ -1,4 +1,18 @@
 package org.ygba.youthgobudget.data.helpers.district;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface DistrictDao {
+    @Insert
+    void saveDistrict(District district);
+    @Query(
+            "SELECT * FROM " + DistrictConstants.TABLE_NAME
+    )
+    LiveData<List<District>> getAllDistricts();
 }
