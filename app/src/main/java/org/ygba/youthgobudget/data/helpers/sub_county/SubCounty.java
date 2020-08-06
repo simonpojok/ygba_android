efb66b01c1cb3d91b32d411067a7ba1c54d0b1a3
simonojok19 <simonojok19@gmail.com>
2020-08-06 19:40:25 +0300

    sub county done

--- app/src/main/java/org/ygba/youthgobudget/data/helpers/sub_county/SubCounty.java
+++ app/src/main/java/org/ygba/youthgobudget/data/helpers/sub_county/SubCounty.java
@@ -1,4 +1,72 @@
 package org.ygba.youthgobudget.data.helpers.sub_county;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = SubCountyConstants.TABLE_NAME)
 public class SubCounty {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = SubCountyConstants.PRIMARY_KEY_COLUMN)
+    private int primaryKey;
+
+    @ColumnInfo(name = SubCountyConstants.SUB_COUNTY_NAME)
+    private String name;
+
+    @ColumnInfo(name = SubCountyConstants.DISTRICT_NAME_COLUMN)
+    private int district;
+
+    @ColumnInfo(name = SubCountyConstants.DISTRICT_ID_COLUMN)
+    private int district_id;
+
+    @ColumnInfo(name = SubCountyConstants.SUB_COUNTY_ID_COLUMN)
+    private int id;
+
+    public int getPrimaryKey() {
+        return primaryKey;
+    }
+
+    public void setPrimaryKey(int primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+
+    public int getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(int district) {
+        this.district = district;
+    }
+
+    public int getDistrict_id() {
+        return district_id;
+    }
+
+    public void setDistrict_id(int district_id) {
+        this.district_id = district_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public SubCounty(String name, int district, int district_id, int id) {
+        this.name = name;
+        this.district = district;
+        this.district_id = district_id;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
---
+++ app/src/main/java/org/ygba/youthgobudget/data/helpers/sub_county/SubCountyConstants.java
@@ -0,0 +1,10 @@
+package org.ygba.youthgobudget.data.helpers.sub_county;
+
+public class SubCountyConstants {
+    public static final String TABLE_NAME = "sub_county";
+    public static final String PRIMARY_KEY_COLUMN = "primary_key";
+    public static final String DISTRICT_ID_COLUMN = "district";
+    public static final String DISTRICT_NAME_COLUMN = "district_name";
+    public static final String SUB_COUNTY_ID_COLUMN = "sub_county";
+    public static final String SUB_COUNTY_NAME = "name";
+}
