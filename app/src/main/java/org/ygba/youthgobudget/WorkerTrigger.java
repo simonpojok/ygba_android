d877c9939c790a03cb270ad80ac378d62e4ba5b1
simonojok19 <simonojok19@gmail.com>
2020-08-08 16:02:13 +0300

    added sub county

--- app/src/main/java/org/ygba/youthgobudget/WorkerTrigger.java
+++ app/src/main/java/org/ygba/youthgobudget/WorkerTrigger.java
@@ -9,6 +9,7 @@ import androidx.work.WorkManager;
 
 import org.ygba.youthgobudget.agriculture.AgricultureUploadWorker;
 import org.ygba.youthgobudget.data.helpers.district.DistrictDownloadWorker;
+import org.ygba.youthgobudget.data.helpers.sub_county.SubCountyDownloadWorker;
 import org.ygba.youthgobudget.social_development.SocialDevelopmentUploadWorker;
 import org.ygba.youthgobudget.water_and_environment.WaterEnvironmentUploadWorker;
 
@@ -23,6 +24,7 @@ public class WorkerTrigger {
         startWaterAndEnvironmentUploadWorker(context);
         startSocialDevelopmentUploadWorker(context);
         startDistrictDownloadWorker(context);
+        startSubCountyDownloadWorker(context);
     }
 
     private static void startAgricultureUploadWorker(Context context) {
@@ -67,6 +69,18 @@ public class WorkerTrigger {
                 .build();
         PeriodicWorkRequest workRequest = new PeriodicWorkRequest.Builder(
                 DistrictDownloadWorker.class,
+                WORKER_TIME_INTERVAL_MINUTES,
+                TimeUnit.MINUTES
+        ).setConstraints(constraints).build();
+        WorkManager.getInstance(context).enqueue(workRequest);
+    }
+
+    private static void startSubCountyDownloadWorker(Context context) {
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build();
+        PeriodicWorkRequest workRequest = new PeriodicWorkRequest.Builder(
+                SubCountyDownloadWorker.class,
                 WORKER_TIME_INTERVAL_MINUTES,
                 TimeUnit.MINUTES
         ).setConstraints(constraints).build();
