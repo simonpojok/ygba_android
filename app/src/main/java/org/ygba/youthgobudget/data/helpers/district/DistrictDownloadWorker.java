e54a50a475aba11477ce89f546524538eea0e621
simonojok19 <simonojok19@gmail.com>
2020-08-08 15:53:43 +0300

    now loading district

--- app/src/main/java/org/ygba/youthgobudget/data/helpers/district/DistrictDownloadWorker.java
+++ app/src/main/java/org/ygba/youthgobudget/data/helpers/district/DistrictDownloadWorker.java
@@ -13,11 +13,14 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.ygba.youthgobudget.YGBARepository;
 import org.ygba.youthgobudget.data.YGBDatabase;
+
+import java.util.List;
 
 import static org.ygba.youthgobudget.utils.Constants.AGRICULTURE_COLLECTION_URL;
 import static org.ygba.youthgobudget.utils.Constants.DISTRICT_COLLECTION_URL;
@@ -41,7 +44,18 @@ public class DistrictDownloadWorker extends Worker {
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        serializeAndSave(response);
+                        DistrictList districtList = new Gson().fromJson(String.valueOf(response), DistrictList.class);
+                        for(final District district: districtList.data) {
+                            YGBDatabase.db_executor.execute(new Runnable() {
+                                @Override
+                                public void run() {
+                                    District district1 = districtDao.getDistrictByID(district.getId());
+                                    if ( district1 == null ) {
+                                       districtDao.saveDistrict(district);
+                                    }
+                                }
+                            });
+                        }
                     }
                 },
                 new Response.ErrorListener() {
@@ -57,29 +71,7 @@ public class DistrictDownloadWorker extends Worker {
         return Result.retry();
     }
 
-    private void serializeAndSave(JSONObject response) {
-        try {
-            final District district = new District(
-                    response.getString("name"),
-                    response.getString("region"),
-                    response.getInt("region_id"),
-                    response.getInt("id")
-            );
-            YGBDatabase.db_executor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    District district1 = districtDao.getDistrictByID(district.getId());
-                    if ( district1 == null ) {
-                        districtDao.saveDistrict(district);
-                    }
-                }
-            });
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
     private static class DistrictList {
-
+        List<District> data;
     }
 }
