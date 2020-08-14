18f630bfb9281ed7e8b6b19bad989d70d2a158ac
simonojok19 <simonojok19@gmail.com>
2020-08-14 16:42:01 +0300

    finished the view holder

--- app/src/main/java/org/ygba/youthgobudget/dialogs/DistrictPickerActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/dialogs/DistrictPickerActivity.java
@@ -1,10 +1,20 @@
 package org.ygba.youthgobudget.dialogs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.ygba.youthgobudget.R;
+import org.ygba.youthgobudget.data.helpers.district.District;
+
+import java.util.List;
 
 public class DistrictPickerActivity extends AppCompatActivity {
 
@@ -12,5 +22,47 @@ public class DistrictPickerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_district_picker);
+    }
+
+    public static  class DistrictAdapter extends RecyclerView.Adapter<DistrictAdapter.DistrictViewHolder> {
+        List<District> districts;
+        Context context;
+
+        public DistrictAdapter(Context context, List<District> districts) {
+            this.districts = districts;
+            this.context = context;
+        }
+
+        @NonNull
+        @Override
+        public DistrictViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(context).inflate(R.layout.item_district, parent, false);
+            return new DistrictViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull DistrictViewHolder holder, int position) {
+            if (districts != null ) {
+                holder.bindView(districts.get(position));
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return districts.size();
+        }
+
+        static class DistrictViewHolder extends RecyclerView.ViewHolder {
+            TextView textView;
+
+            public DistrictViewHolder(@NonNull View itemView) {
+                super(itemView);
+                textView = itemView.findViewById(R.id.district_name);
+            }
+
+            public void bindView(District district) {
+                textView.setText(district.getName());
+            }
+        }
     }
 }
