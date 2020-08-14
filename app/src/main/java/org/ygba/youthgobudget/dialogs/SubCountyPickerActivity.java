0ffa648846c2c1764c6e0118c505d2d14e790e18
simonojok19 <simonojok19@gmail.com>
2020-08-14 17:02:55 +0300

    added sub county activities

---
+++ app/src/main/java/org/ygba/youthgobudget/dialogs/SubCountyPickerActivity.java
@@ -0,0 +1,16 @@
+package org.ygba.youthgobudget.dialogs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.ygba.youthgobudget.R;
+
+public class SubCountyPickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_county_picker);
+    }
+}
