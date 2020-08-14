e277acff0274c1e5e08f126a38016678888642c1
simonojok19 <simonojok19@gmail.com>
2020-08-14 16:08:11 +0300

    adding district recyclerview

--- app/src/main/java/org/ygba/youthgobudget/dialogs/DistrictPickerActivity.kt
+++ app/src/main/java/org/ygba/youthgobudget/dialogs/DistrictPickerActivity.kt
@@ -1,12 +1,49 @@
 package org.ygba.youthgobudget.dialogs
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import org.ygba.youthgobudget.R
+import org.ygba.youthgobudget.data.helpers.district.District
 
 class DistrictPickerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_district_picker)
+
+        val districtRecyclerView: RecyclerView = findViewById(R.id.district_recycler_view);
+        val districtAdapter: DistrictAdapter = DistrictAdapter(this);
+
+    }
+
+    class DistrictAdapter(val context: Context): RecyclerView.Adapter<DistrictAdapter.DistrictViewHolder>() {
+        lateinit var districts: List<District>;
+
+        class DistrictViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+            val textView: TextView = itemView.findViewById(R.id.district_name);
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DistrictViewHolder {
+            val view = LayoutInflater.from(context).inflate(R.layout.item_district, parent, false);
+            return DistrictViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return districts.size
+        }
+
+        override fun onBindViewHolder(holder: DistrictViewHolder, position: Int) {
+            holder.textView.text = districts[position].name
+        }
+
+        fun loadDistricts(districtList: List<District>) {
+            districts = districtList;
+            notifyDataSetChanged()
+        }
     }
 }
