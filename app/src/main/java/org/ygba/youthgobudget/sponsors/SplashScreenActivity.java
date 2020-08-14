c261251ccfa1299c0906c92ce6bfd9edede0f9aa
simonojok19 <simonojok19@gmail.com>
2020-08-14 21:35:50 +0300

    added some codes

---
+++ app/src/main/java/org/ygba/youthgobudget/sponsors/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package org.ygba.youthgobudget.sponsors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.ygba.youthgobudget.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
