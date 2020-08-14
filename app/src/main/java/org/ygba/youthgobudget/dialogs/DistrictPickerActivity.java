6bf64ebeebdb4b383a6c689ec232b5e47e17fba6
simonojok19 <simonojok19@gmail.com>
2020-08-14 17:44:03 +0300

    finished the district picker as well

--- app/src/main/java/org/ygba/youthgobudget/dialogs/DistrictPickerActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/dialogs/DistrictPickerActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,14 +21,16 @@ import org.ygba.youthgobudget.data.helpers.district.District;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-public class DistrictPickerActivity extends AppCompatActivity {
+public class DistrictPickerActivity extends AppCompatActivity implements DistrictPickerAdapter.OnDistrictClickListener {
+    public static final String DISTRICT_NAME = "org.ygba.youthgobudget.dialogs.DistrictPickerActivity.DISTRICT_NAME";
+    public static final String DISTRICT_ID = "org.ygba.youthgobudget.dialogs.DistrictPickerActivity.DISTRICT_ID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_district_picker);
         RecyclerView recyclerView = findViewById(R.id.district_recycler_view);
-        DistrictAdapter districtAdapter = new DistrictAdapter(this, getDistricts());
+        DistrictPickerAdapter districtAdapter = new DistrictPickerAdapter(this, getDistricts());
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(districtAdapter);
     }
@@ -39,5 +42,14 @@ public class DistrictPickerActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return null;
+    }
+
+    @Override
+    public void onDistrictClick(District district) {
+        Intent intent = new Intent();
+        intent.putExtra(DISTRICT_NAME, district.getName());
+        intent.putExtra(DISTRICT_ID, district.getId());
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
