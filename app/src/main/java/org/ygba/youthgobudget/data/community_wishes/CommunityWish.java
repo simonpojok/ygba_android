87eebcc0bd6f573e04d9749bf9933d7ba569cf2a
simonojok19 <simonojok19@gmail.com>
2020-08-14 20:51:09 +0300

    finishing community wishes

--- app/src/main/java/org/ygba/youthgobudget/data/community_wishes/CommunityWish.java
+++ app/src/main/java/org/ygba/youthgobudget/data/community_wishes/CommunityWish.java
@@ -1,4 +1,7 @@
 package org.ygba.youthgobudget.data.community_wishes;
 
+import androidx.room.Entity;
+
+@Entity(tableName = CommunityWishConstants.TABLE_NAME)
 public class CommunityWish {
 }
--- app/src/main/java/org/ygba/youthgobudget/data/community_wishes/CommunityWishConstants.java
+++ app/src/main/java/org/ygba/youthgobudget/data/community_wishes/CommunityWishConstants.java
@@ -1,4 +1,5 @@
 package org.ygba.youthgobudget.data.community_wishes;
 
 public class CommunityWishConstants {
+    public static final String TABLE_NAME = "table_name_community";
 }
