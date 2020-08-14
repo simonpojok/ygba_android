7a82f2f7033be89849bf639a6a32b9d5ebe54764
simonojok19 <simonojok19@gmail.com>
2020-08-14 16:22:02 +0300

    added the dsitrict view model

---
+++ app/src/main/java/org/ygba/youthgobudget/dialogs/DistrictPickerViewModel.kt
@@ -0,0 +1,7 @@
+package org.ygba.youthgobudget.dialogs
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class DistrictPickerViewModel(application: Application) : AndroidViewModel(application) {
+}
