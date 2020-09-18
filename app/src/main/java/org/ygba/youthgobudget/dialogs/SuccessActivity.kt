88938534c59da14c6de0abf7375fcfd51c2518eb
simonojok19 <simonojok19@gmail.com>
2020-09-18 14:10:21 +0300

    added saved

--- app/src/main/java/org/ygba/youthgobudget/dialogs/SuccessActivity.kt
+++ app/src/main/java/org/ygba/youthgobudget/dialogs/SuccessActivity.kt
@@ -1,9 +1,11 @@
 package org.ygba.youthgobudget.dialogs
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.ygba.youthgobudget.R
 
-class SuccessActivity : AppCompatActivity() {
+class SuccessActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_success)
