6d5e32b03c0f797ccf232016398aebce9318ce52
simonojok19 <simonojok19@gmail.com>
2020-09-18 15:03:21 +0300

    added do work

--- app/src/main/java/org/ygba/youthgobudget/agriculture/AgricultureUploadWorker.java
+++ app/src/main/java/org/ygba/youthgobudget/agriculture/AgricultureUploadWorker.java
@@ -21,6 +21,7 @@ import org.ygba.youthgobudget.YGBARepository;
 import org.ygba.youthgobudget.data.YGBDatabase;
 import org.ygba.youthgobudget.data.agriculture.AgricultureDao;
 import org.ygba.youthgobudget.data.agriculture.AgricultureQuestion;
+import org.ygba.youthgobudget.utils.networktask.AgricultureNetworkTask;
 
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -43,97 +44,7 @@ public class AgricultureUploadWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-
-        try {
-            List<AgricultureQuestion> agricultureQuestions = getAgricultureQuestion();
-
-            for (AgricultureQuestion agricultureQuestion: agricultureQuestions) {
-
-                JSONObject body = new JSONObject();
-                body.put("record_id", agricultureQuestion.getId());
-                body.put("financial_year", agricultureQuestion.getFinancialYear());
-                body.put("date_recorded", agricultureQuestion.getDate());
-                body.put("village", agricultureQuestion.getVillage());
-                body.put("parish", agricultureQuestion.getParish());
-                body.put("subcounty", agricultureQuestion.getSubCounty());
-                body.put("staff_name", agricultureQuestion.getAgentName());
-                body.put("phone_id", agricultureQuestion.getAgentNumber());
-                body.put("staff_number", agricultureQuestion.getAgentNumber());
-                body.put("quarter", agricultureQuestion.getQuarter());
-                body.put("gender", "gender");
-                body.put("is_there_substantive_agricultural_extension_worker", agricultureQuestion.getQuestion1Objective());
-                body.put("if_no_why", agricultureQuestion.getQuestion1Reason());
-                body.put("extension_services_expected_or_approved", agricultureQuestion.getQuestion2ExtensionExpectedAmount());
-                body.put("extension_services_amount_received", agricultureQuestion.getQuestion2ExtensionAmountReceived());
-                body.put("extension_services_date_received", agricultureQuestion.getQuestion2ExtensionDateReceived());
-                body.put("extension_services_date_withdrawn", agricultureQuestion.getQuestion2ExtensionDateWithdram());
-                body.put("development_expected_or_approved", agricultureQuestion.getQuestion2DevelopmentAmountExpected());
-                body.put("development_amount_received", agricultureQuestion.getQuestion2DevelopmentAmountReceived());
-                body.put("development_date_received", agricultureQuestion.getQuestion2DevelopmentDateReceived());
-                body.put("development_date_withdrawn", agricultureQuestion.getQuestion2DevelopmentDateWithdrawn());
-                body.put("number_of_field_visits_for_farmer_support", agricultureQuestion.getAnswerQuestion2_1());
-                body.put("advisory_demonstration_community", agricultureQuestion.getAnswerQuestion2_2());
-                body.put("if_yes_how_many_during_the_quarter", agricultureQuestion.getAnswerQuestion2_3());
-                body.put("areas_where_the_meetings_or_workshops_were_held", agricultureQuestion.getAnswerQuestion2_4());
-                body.put("reasons_for_not_conducting_the_community_meeting", agricultureQuestion.getAnswerQuestion2_5());
-                body.put("are_there_any_advisory_services", agricultureQuestion.getAnswerQuestion3_1());
-                body.put("if_yes_how_many_were_done_during_the_quarter", agricultureQuestion.getAnswerQuestion3_2());
-                body.put("mention_the_areas_where_the_visits_were_made", agricultureQuestion.getAnswerQuestion3_3());
-                body.put("female_number_of_farmers_visited_for_advisory_services", agricultureQuestion.getAnswerQuestion3_4_Female());
-                body.put("male_number_of_farmers_visited_for_advisory_services", agricultureQuestion.getAnswerQuestion3_4_Male());
-                body.put("reasons_for_not_conducting_the_farmer_advisory_services_visits", agricultureQuestion.getAnswerQuestion3_5());
-                body.put("reasons_for_no_agriculture_inputs_and_livestock", agricultureQuestion.getAnswerQuestion4_3_reason());
-                body.put("any_other_observations_or_challenges", agricultureQuestion.getAnswerQuestion4_3_otherReason());
-//https://ygba.org/api/collect/agric
-
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                        Request.Method.POST,
-                        AGRICULTURE_COLLECTION_URL,
-                        body,
-                        new Response.Listener<JSONObject>() {
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                Log.d("Agr", response.toString());
-                                try {
-                                    int recordId = response.getInt("record_id");
-                                    deleteAgricultureQuestion(recordId);
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                Log.d("Error", error.toString());
-                            }
-                        }
-                );
-
-                RequestQueue requestQueue = Volley.newRequestQueue(context);
-                requestQueue.add(jsonObjectRequest);
-            }
-
-
-        } catch (JSONException | ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-            return Result.failure();
-        }
-        return Result.retry();
-    }
-
-    private void deleteAgricultureQuestion(int record_id) {
-        ygbaRepository.getAgricultureQuestionByIdAndDelete(record_id);
-    }
-
-    private List<AgricultureQuestion> getAgricultureQuestion() throws ExecutionException, InterruptedException {
-        Callable<List<AgricultureQuestion>> listCallable = new Callable<List<AgricultureQuestion>>() {
-
-            @Override
-            public List<AgricultureQuestion> call() throws Exception {
-                return agricultureDao.getAgricultureQuestionForUpload(true);
-            }
-        };
-        return YGBDatabase.db_executor.submit(listCallable).get();
+        new AgricultureNetworkTask(context, agricultureDao, ygbaRepository).doAgricultureNetworkTask();
+        return Result.success();
     }
 }
