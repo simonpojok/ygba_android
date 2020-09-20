4fa841a9c3374042defaf96f47bd558e9b96cc2e
simonojok19 <simonojok19@gmail.com>
2020-09-20 11:59:24 +0300

    added communty wishes

--- app/src/main/java/org/ygba/youthgobudget/community_wishes/CommunityWishesActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/community_wishes/CommunityWishesActivity.java
@@ -1,6 +1,16 @@
 package org.ygba.youthgobudget.community_wishes;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.ygba.youthgobudget.R;
+
 public class CommunityWishesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community_wish);
+    }
 }
