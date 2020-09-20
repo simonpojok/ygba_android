480eaab76629a281cae6b8ac9df0aa9c0a169635
simonojok19 <simonojok19@gmail.com>
2020-09-20 12:00:27 +0300

    declared some fields

--- app/src/main/java/org/ygba/youthgobudget/community_wishes/CommunityWishesActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/community_wishes/CommunityWishesActivity.java
@@ -1,6 +1,10 @@
 package org.ygba.youthgobudget.community_wishes;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +12,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import org.ygba.youthgobudget.R;
 
 public class CommunityWishesActivity extends AppCompatActivity {
+    private   final int DISTRICT_NAME_REQUESTER_CODE = 1;
+    private   final int SUB_COUNTY_NAME_REQUEST_CODE = 2;
+    private  final int QUESTION_2_DATE_RECEIVED_1_REQUEST_CODE = 3;
+    private  final int  QUESTION_2_DATE_WITHDRAWN_1_REQUEST_CODE = 4;
+    private  final int  QUESTION_2_DATE_RECEIVED_2_REQUEST_CODE = 5;
+    private  final int  QUESTION_2_DATE_WITHDRAWN_2_REQUEST_CODE = 6;
+    private  final int  QUESTION_4_DATE_WITHDRAWN_5_REQUEST_CODE = 8;
+    private final int QUESTION_4_DATE_WITHDRAWN_4_REQUEST_CODE = 9;
+    private final int QUESTION_4_DATE_WITHDRAWN_3_REQUEST_CODE = 10;
+    private final int QUESTION_4_DATE_WITHDRAWN_2_REQUEST_CODE = 11;
+    private final int QUESTION_4_DATE_WITHDRAWN_1_REQUEST_CODE = 12;
+    private final int DISPLAY_SUCCESS_MESSAGE_ACTIVITY = 13;
+    private int districtId = 0;
+    RadioGroup question1RadioGroup;
+    Spinner quarterSpinner;
+    Spinner financialSpinner;
+
+    EditText financialYear;
+    TextView districtText;
+
+    EditText villageEditText;
+    EditText parishTextEdit;
+    TextView divisionEditText;
+    EditText agentFullNameEditText;
+    EditText agentTelephoneEditText;
+    EditText agentNumberEditText;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
